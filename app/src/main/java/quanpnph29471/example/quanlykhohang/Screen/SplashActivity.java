a37948df09f199114b8dc46b850df5c73f839d0a
QuanPN06 <quanpnph29471@fpt.edu.vn>
2023-11-18 16:42:35 +0700

    init default

--- app/src/main/java/quanpnph29471/example/quanlykhohang/Screen/SplashActivity.java
+++ app/src/main/java/quanpnph29471/example/quanlykhohang/Screen/SplashActivity.java
@@ -2,15 +2,38 @@ package quanpnph29471.example.quanlykhohang.Screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import quanpnph29471.example.quanlykhohang.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView img;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        img = findViewById(R.id.img_splash);
+        tv = findViewById(R.id.tv_splash);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.splash_anim);
+        img.startAnimation(animation);
+        tv.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
     }
+
 }
